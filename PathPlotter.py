f6bdefc7a031c846d50fd7d60dedd44617a24b6d
root <root@SURFACE.localdomain>
2018-03-05 21:13:13 +0100

    added PathPlotter

---
+++ PathPlotter.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.lines as lines
+
+print('Drawing last accepted path')
+
+with open('cities.txt') as cityfile:
+	tempcitylist = cityfile.read().splitlines()
+
+citylist = list()
+
+for el in tempcitylist:
+	citylist.append(el.split(' '))
+	
+for i in range(len(citylist)):
+	citylist[i][0] = int(citylist[i][0])
+	citylist[i][1] = int(citylist[i][1])
+
+with open('path.txt') as pathfile:
+	pathlist = pathfile.read().splitlines()
+pathlist = [int(i) for i in pathlist]
+	
+pointlist = list()
+for i in range(len(citylist)):
+	pointlist.append(citylist[pathlist[i]])
+
+
+data = np.array(pointlist)
+plt.plot(data[:, 0], data[:, 1])
+plt.show()
